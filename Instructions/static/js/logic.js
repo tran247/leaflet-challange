e089660b197a9249b4d850d6ae408bcc4006649d
tran247 <75042393+tran247@users.noreply.github.com>
2021-05-28 22:38:59 -0400

    asked classmate for help

--- Instructions/static/js/logic.js
+++ Instructions/static/js/logic.js
@@ -1,11 +1,10 @@
-// Used day 1 activity 10 code 
+// some code is from activity 10
 
 // Store our API endpoint inside queryUrl
-var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=" +
-  "2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 // Perform a GET request to the query URL
-d3.json(queryUrl).then(function(data) {
+d3.json(queryUrl, function(data) {
   // Once we get a response, send the data.features object to the createFeatures function
   createFeatures(data.features);
 });
@@ -16,40 +15,109 @@ function createFeatures(earthquakeData) {
   // Give each feature a popup describing the place and time of the earthquake
   function onEachFeature(feature, layer) {
     layer.bindPopup("<h3>" + feature.properties.place +
-      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" + "<h4> Magnitude: " + feature.properties.mag +"</h4>");
   }
 
-  // Create a GeoJSON layer containing the features array on the earthquakeData object
-  // Run the onEachFeature function once for each piece of data in the array
+
+// Function for Circle Color Base on Criteria. The Color Scale is base of the 7 colors of a Rainboy ROY G BIV
+function QuakeColor(Qcolor) {
+    switch(true) {
+        case (0 <= Qcolor && Qcolor <= 1.0):
+          return "Red";
+        case (1.0 <= Qcolor && Qcolor <= 2.0):
+            return "Orange";
+        case (2.0 <= Qcolor && Qcolor<= 3.0):
+          return "Yellow";
+        case (3.0 <= Qcolor && Qcolor<= 4.0):
+            return "Green";
+        case (4.0 <= Qcolor && Qcolor<= 5.0):
+            return "Blue";
+        case (5.0 <= Qcolor && Qcolor <= 6.0):
+          return "Indigo";
+        default:
+          return "Violet";
+    }
+  }
+//   Create a circle function
+
+function CircleMaker(features, latlng){
+    var CircleOptions = {
+        radius: features.properties.mag * 8,
+        fillColor: QuakeColor(features.properties.mag),
+        color: QuakeColor(features.properties.mag),
+        opacity: 1.0,
+        fillOpacity: .5
+
+    }
+    return L.circleMarker(latlng, CircleOptions)
+}
+  // Create a GeoJSON 
   var earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
+    onEachFeature: onEachFeature,
+    pointToLayer: CircleMaker
   });
 
-  // Sending our earthquakes layer to the createMap function
+  // earthquakes layer 
   createMap(earthquakes);
 }
 
-// make a function to show color of earthquakes
-function colorQuakes(magnitude) [
-    var color = "";
-    if (magnitude > 5) {
-        color = "#f06b6b";
-    }
-    else if (magnitude > 4) {
-        color = "#f0a76b";
-    }
-    else if (magnitude > 3) {
-        color = "#f3db4d"
-    }
-    else if (magnitude > 2) {
-        color = "elf34d"
-    }
-    else {
-        color = "b7f34d";
-    }
-    return color;
+function createMap(earthquakes) {
+
+  // streetmap and Satellite layers
+  var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  });
+
+  var Satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "satellite-v9",
+    accessToken: API_KEY
+  });
+
+
+  var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "dark-v10",
+    accessToken: API_KEY
+  });
+  // base layers
+  var baseMaps = {
+    "Street Map": streetmap,
+    "Light Map": lightmap,
+    "Satellite Map": Satellite
+  };
+
+  // overlay layer
+  var overlayMaps = {
+    Earthquakes: earthquakes
+  };
+
+  // Create our map
+  var myMap = L.map("mapid", {
+    center: [
+      37.09, -95.71
+    ],
+    zoom: 5,
+    layers: [streetmap, earthquakes]
+  });
+
+// display information about our map
+var info = L.control({
+    position: "bottomright"
+  });
+// Add legend
+info.addTo(myMap);
+
+  // Create a layer control
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+
 }
-
-
-
-});
